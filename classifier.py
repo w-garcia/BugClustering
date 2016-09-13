199a51951a816b1a98630dcb5ba27c8be8b55f5f
w-garcia <wgarcia2013@fau.edu>
2016-09-13 19:11:28 -0400

    Fixed 'all_systems' use case for label datasets

--- classifier.py
+++ classifier.py
@@ -141,7 +141,10 @@ def setup_datasets(slice):
         _dataset_stack = [row for row in DBModel.LFF_Keywords.get_db_ref_by_system(cfg.labelling_dataset).select()]
         # Label dataset should never intersect with model data
         if cfg.model_selection == 'all_systems':
-            selection_cache = [row for row in DBModel.LFF_Keywords.select()]
+            selection_cache = []
+            for system in util.systems:
+                for row in DBModel.LFF_Keywords.get_db_ref_by_system(system).select():
+                    selection_cache.append(row)
         else:
             selection_cache = [row for row in DBModel.LFF_Keywords.get_db_ref_by_system(cfg.model_selection).select()]
         return _dataset_stack, selection_cache
